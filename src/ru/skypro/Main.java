cf150755229523e313f9a9a6ebd1ca14b1108d6d
ArtemCH240322 <sun2383@mail.ru>
2022-04-20 15:30:37 +0300

    Первый комит. Практические задания - закончил.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,28 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String phone = "+920-111-22-33";
+        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
+        if (phone.length() == 10) {
+//            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телефон слишком короткий");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Среди нас посторонний!!!");
+        }
+        System.out.println("phone =" + phone);
+        String expektedPhon = "79201112233";
+        if (phone.equals(expektedPhon)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
     }
-}
+
+
+    }
+
